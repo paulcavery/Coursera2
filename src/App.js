c1be9d135a20de88eb59c14a0301837540dcdf05
Paul Avery Jr <paul.c.avery@gmail.com>
2020-12-11 20:15:27 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { render } from 'react-dom';
 import { Component } from 'react';
+import Menu from './Components/MenuComponent'
 
 class App extends Component{
   render() {
-  return (
+     return (
     <div className="App">
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
   };
