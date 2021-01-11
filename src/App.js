5fd6e7f17414f5c8a9fd4daf2a56b8aadea744fb
Paul Avery Jr <paul.c.avery@gmail.com>
2021-01-11 11:55:41 -0500

    Project week 3-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import { Component } from "react";
 import Main from "./Components/MainComponent";
 import "./App.css";
-import { BrowserRouter } from "react-router-d
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
 
 const store = ConfigureStore();
-
 
 class App extends Component {
 	render() {
 		return (
-	<Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Main />
-          </div>
-        </BrowserRouter>
-      </Provider>
+			<Provider store={store}>
+				<BrowserRouter>
+					<div className="App">
+						<Main />
+					</div>
+				</BrowserRouter>
+			</Provider>
 		);
 	}
 }
--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
 	Card,
 	CardImg,
@@ -7,8 +7,20 @@ import {
 	CardTitle,
 	Breadcrumb,
 	BreadcrumbItem,
+	Col,
+	Row,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Label,
+	Button,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderDish({ dish }) {
 	// make sure the selected dish is an existing dish
@@ -54,6 +66,7 @@ function RenderComment({ comments }) {
 		<div>
 			<h4 className="mb-1">Comments</h4>
 			{commentView}
+			<CommentForm />
 		</div>
 	);
 }
@@ -90,3 +103,146 @@ const DishDetail = (props) => {
 };
 
 export default DishDetail;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isModalOpen: false,
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
+	}
+
+	handleSubmit(values) {
+		console.log(values);
+		this.toggleModal();
+	}
+
+	toggleModal = (event) => {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<Button
+					onClick={this.toggleModal}
+					style={{ background: "white", color: "black" }}
+				>
+					<span className="fa fa-pencil fa-lg"></span> Submit Comment
+				</Button>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader
+						isOpen={this.state.isModalOpen}
+						toggle={this.toggleModal}
+					>
+						Submit Comment
+					</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+							<Row className="form-group">
+								<Label htmlFor="rating" md={2}>
+									Rating
+								</Label>
+								<Col md={10}>
+									<Control.select
+										model=".rating"
+										id="rating"
+										name="rating"
+										placeholder="Rating"
+										className="form-control"
+										validators={{
+											required,
+										}}
+									>
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+									<Errors
+										className="text-danger"
+										model=".rating"
+										show="touched"
+										messages={{
+											required: "Required",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="author" md={2}>
+									Author
+								</Label>
+								<Col md={10}>
+									<Control.text
+										model=".author"
+										id="author"
+										name="author"
+										placeholder="Author"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".author"
+										show="touched"
+										messages={{
+											required: "Required",
+											minLength: "Must be greater than 3 characters",
+											maxLength: "Must be 15 characters or less",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="message" md={2}>
+									Comment
+								</Label>
+								<Col md={10}>
+									<Control.textarea
+										style={{ height: "200px" }}
+										model=".message"
+										id="message"
+										name="message"
+										placeholder="Message"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".message"
+										show="touched"
+										messages={{
+											required: "Required",
+											minLength: "Must be greater than 3 characters",
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col sm={{ size: 2, offset: 2 }}>
+									<Button type="submit" color="primary">
+										Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</React.Fragment>
+		);
+	}
+}
